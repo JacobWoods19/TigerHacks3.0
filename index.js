ba657af40985751e1e712f09b8286c44c6a65072
Jacob Woods <68571928+jacobwoods45@users.noreply.github.com>
2022-11-06 07:26:37 -0600

    asdfkl

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function report(selection) {
     position = navigator.geolocation.getCurrentPosition(send_data)
 }
 
-const backend_url = 'https://clownfish-app-y6vt9.ondigitalocean.app/'
+const backend_url = 'https://clownfish-app-y6vt9.ondigitalocean.app'
 
 function send_data (position) {
     const date = new Date();
@@ -19,21 +19,21 @@ function send_data (position) {
 
 
 
-function verify_user() {
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
+// function verify_user() {
+//     var email = document.getElementById("email").value;
+//     var password = document.getElementById("password").value;
 
-    $.post( backend_url + "/verify_user", 
-    { email: email, password: password })    
-        .done(function(msg){ 
-            window.location = "buttons.html"; // Redirecting to other page.
-         })
-        .fail(function(xhr, status, error) {
-            // error handling
+//     $.post( backend_url + "/verify_user", 
+//     { email: email, password: password })    
+//         .done(function(msg){ 
+//             window.location = "buttons.html"; // Redirecting to other page.
+//          })
+//         .fail(function(xhr, status, error) {
+//             // error handling
             
-        });
+//         });
 
-}
+// }
 
 
 
@@ -42,12 +42,33 @@ function verify_user() {
 legit_email = 'user@gmail.com'
 legit_password = 'password'
 function validate_login() {
+    console.log("Validating login")
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     var credentials_error = document.getElementById("credentials_error");
 
-    if ( email == legit_email && password == legit_password){
-        window.location = "buttons.html"; // Redirecting to other page.
-        return false;
-    } 
+    //make network request to backend to verify user
+    verify_user(email, password)
+    
+    
+    
 }
+const verify_user = async (email, password) => {
+    const response = await fetch(backend_url + "/verify_user", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: email, password: password }),
+    });
+    const data = await response.json();
+    //if status is success, redirect to buttons.html else flash error message
+    if (data.status == "success") {
+        window.location = "buttons.html";
+    }
+    else {
+        alert("Invalid credentials")
+    }
+    
+}
+
