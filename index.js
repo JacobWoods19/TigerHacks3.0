cb648f1af8fa5f06c0f5fb6d527439318be9b4eb
Jacob Woods <68571928+jacobwoods45@users.noreply.github.com>
2022-11-06 08:17:49 -0600

    fdasf

--- index.js
+++ index.js
@@ -5,6 +5,34 @@ function report(selection) {
 const backend_url = 'https://clownfish-app-y6vt9.ondigitalocean.app'
 
 
+function getLocation() {
+
+    if (navigator.geolocation) {
+
+      navigator.geolocation.getCurrentPosition(handle_location);
+      
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function handle_location(position) {
+    localStorage.setItem("lat", position.coords.latitude);
+    localStorage.setItem("lon", position.coords.longitude);
+}
+
+function create_report(incident_type) {
+    var lat = localStorage.getItem("lat");
+    var lon = localStorage.getItem("lon");
+    var email = localStorage.getItem("email");
+    var data = {
+        "incident_type": incident_type,
+        "lat": lat,
+        "lon": lon,
+        "email": email
+    }
+    submit_incident(data);
+}
 
 
 
@@ -28,8 +56,6 @@ const backend_url = 'https://clownfish-app-y6vt9.ondigitalocean.app'
 
 
 
-legit_email = 'user@gmail.com'
-legit_password = 'password'
 function validate_login() {
     console.log("Validating login")
     var email = document.getElementById("email").value;
@@ -39,23 +65,23 @@ function validate_login() {
     //make network request to backend to verify user
     verify_user(email, password)
     console.log("email: " + email + " password: " + password)
-    
-    
-    
 }
 //submit incident to backend
-const submit_incident = async (long, lat, email, date) => {
-    const response = await fetch(backend_url + "/add_incident", {
+const submit_incident = async (data) => {
+    // build url with query params
+    // example: https://clownfish-app-y6vt9.ondigitalocean.app/add_incident?date=2022-10-12&lat=38.9590&long=-92.3233&incident_type=Pothole&email=Don_Waters@gmail.com
+    var url = backend_url + "/add_incident?date=" + data.date + "&lat=" + data.lat + "&long=" + data.lon + "&incident_type=" + data.incident_type + "&email=" + data.email
+    const response = await fetch(url, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
 
-        body: JSON.stringify({long: long, lat: lat, email: email, date: date}),
+        body: JSON.stringify({long: data.long, lat: data.lat, email: data.email, date: data.date}),
     });
-    const data = await response.json();
-    console.log(data)
-    return data;
+    const return_data = await response.json();
+    console.log(return_data)
+    return return_data;
 }
 
 const verify_user = async (email, password) => {
@@ -79,4 +105,4 @@ const verify_user = async (email, password) => {
     }
     
 }
-
+getLocation()
